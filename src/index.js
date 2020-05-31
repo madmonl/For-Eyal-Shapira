c6ce5686f9300b758d91737544009a2054063cd5
liad madmon <madmonl@post.bgu.ac.il>
2020-05-31 19:35:03 +0300

    Add styles

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './components/store/configureStore';
 import { login, logout } from './components/auth/authSlice';
 import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
 import LoadingPage from './components/LoadingPage';
